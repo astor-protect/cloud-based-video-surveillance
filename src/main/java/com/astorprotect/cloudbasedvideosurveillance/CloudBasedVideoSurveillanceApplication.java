1dc5679e294a7158f83bf906c535acf91397a64c
FanJups <jupsfan@gmail.com>
2019-08-29 09:40:35 +0200

    First programming files

---
+++ src/main/java/com/astorprotect/cloudbasedvideosurveillance/CloudBasedVideoSurveillanceApplication.java
@@ -0,0 +1,13 @@
+package com.astorprotect.cloudbasedvideosurveillance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBasedVideoSurveillanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBasedVideoSurveillanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astorprotect/cloudbasedvideosurveillance/CloudBasedVideoSurveillanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.astorprotect.cloudbasedvideosurveillance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudBasedVideoSurveillanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
