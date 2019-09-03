9bce92d8c94458f5fac6b1709a842b47a7ba090a
FanJups <jupsfan@gmail.com>
2019-09-03 13:14:44 +0200

    Adding Dockerfile

---
+++ src/main/java/com/astorprotect/cloudbasedvideosurveillance/configuration/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.astorprotect.cloudbasedvideosurveillance.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+ /**
+  * disable spring boot security default  login page
+  * */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+    protected void configure(HttpSecurity security) throws Exception
+    {
+     security.httpBasic().disable();
+    }
+
+}
